588425f86faea8903a7d7060e466bf1f64a9f162
artagny <itho.dev11.gny@gmail.com>
2026-02-02 09:25:21 +0700

    package

---
+++ src/features/sync/types/index.ts
@@ -0,0 +1,22 @@
+export type Event = {
+    id: string;
+    nama_event: string;
+    mulai: Date;
+    selesai: Date;
+    createdAt: Date;
+    jadwal: Jadwal[];
+}
+
+export type Jadwal = {
+    id: string;
+    nama_jadwal: string;
+    mulai: Date;
+    selesai: Date;
+    token: string;
+    batas_mulai_mengerjakan: Date;
+    selesai_tepat_waktu: boolean;
+}
+
+export type PaketSoal = {
+    
+}
