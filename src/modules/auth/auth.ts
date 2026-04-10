79d490f364c08969ca5e668c7973c98a98ed4295
artagny <itho.dev11.gny@gmail.com>
2026-04-10 07:08:37 +0700

    fix cookie set

--- src/modules/auth/auth.ts
+++ src/modules/auth/auth.ts
@@ -28,7 +28,6 @@ export const auth = betterAuth({
         defaultCookieAttributes: {
             sameSite: "none",
             secure: true,
-            partitioned: true
         }
     },
 });
