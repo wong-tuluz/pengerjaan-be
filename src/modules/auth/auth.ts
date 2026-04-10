7e1b450eba25a19c107d647afb7e15cdbddb2411
artagny <itho.dev11.gny@gmail.com>
2026-04-10 07:19:03 +0700

    fix cookie set

--- src/modules/auth/auth.ts
+++ src/modules/auth/auth.ts
@@ -29,7 +29,7 @@ export const auth = betterAuth({
             domain: "antz.biz.id",
         },
         defaultCookieAttributes: {
-            sameSite: "none",
+            sameSite: "lax",
             secure: true,
         }
     },
